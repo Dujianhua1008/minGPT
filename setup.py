820a97cefa95c1ab4fb0e767f08c9797e80c6e3e
Dujianhua1008 <tulipdu1008@gmail.com>
2023-10-11 16:28:44 +0800

    Changes新增文件

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from setuptools import setup
+# from setuptools import setup
+#
+# setup(name='minGPT',
+#       version='0.0.1',
+#       author='Andrej Karpathy',
+#       packages=['mingpt'],
+#       description='A PyTorch re-implementation of GPT',
+#       license='MIT',
+#       install_requires=[
+#             'torch',
+#       ],
+# )
 
-setup(name='minGPT',
-      version='0.0.1',
-      author='Andrej Karpathy',
-      packages=['mingpt'],
-      description='A PyTorch re-implementation of GPT',
-      license='MIT',
-      install_requires=[
-            'torch',
-      ],
-)
+
+def merge( intervals):
+        intervals.sort(key=lambda x: x[0])
+        x=intervals.pop(0)
+        result=[]
+        while intervals :
+            temp=intervals[0]
+            a=x[0];b=x[1];c=temp[0];d=temp[1];
+            if c>b+1 or d<a-1:
+                result.append(x)
+                x=temp
+            elif (c>=a and c<=b)and(d>=b):
+                x=[a,d]
+            elif (c<=a and d>=b ):
+                x=[c,d]
+            elif c>=a and d<=b:
+                x=[a,b]
+            elif c<=a and (d<=b and d>=a):
+                x=[c,b]
+            intervals.pop(0)
+        result.append(x)
+        return result
+nums=[[1,4],[0,0]]
+print(merge(nums))
